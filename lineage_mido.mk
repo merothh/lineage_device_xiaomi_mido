927dbd78fd36e0d32fd2ab092b5ece129e561cf8
Bruno Martins <bgcngm@gmail.com>
2018-08-10 13:47:04 +0100

    mido: Consider lineage_mido product via the AOSP way
    Change-Id: Ibdeb62f0aa30933a09ac12ca825025e5f18574ab

--- lineage_mido.mk
+++ lineage_mido.mk
@@ -14,12 +14,22 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/mido/full_mido.mk)
-
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from mido device
+$(call inherit-product, device/xiaomi/mido/device.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_mido
+PRODUCT_DEVICE := mido
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 4
+PRODUCT_MANUFACTURER := Xiaomi
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -29,3 +39,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys"
+
+TARGET_VENDOR := Xiaomi
